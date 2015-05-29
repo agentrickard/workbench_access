0a5669b689f102aa4500f1e8671164b17aa68bdb
agentrickard <agentrickard@gmail.com>
2015-05-29 18:22:33 -0400

    Adds settings for label text when referring to sections in the UI.

--- src/Form/WorkbenchAccessConfigForm.php
+++ src/Form/WorkbenchAccessConfigForm.php
@@ -7,13 +7,47 @@
 
 namespace Drupal\workbench_access\Form;
 
-use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\State\StateInterface;
+use Drupal\Core\Form\ConfigFormBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Configure Workbench access settings for this site.
  */
 class WorkbenchAccessConfigForm extends ConfigFormBase {
+
+  /**
+   * The state keyvalue collection.
+   *
+   * @var \Drupal\Core\State\StateInterface
+   */
+  protected $state;
+
+  /**
+   * Constructs a new WorkbenchAccessConfigForm.
+   *
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The factory for configuration objects.
+   * @param \Drupal\Core\State\StateInterface $state
+   *   The state keyvalue collection to use.
+   */
+  public function __construct(ConfigFactoryInterface $config_factory, StateInterface $state) {
+    parent::__construct($config_factory);
+    $this->state = $state;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('config.factory'),
+      $container->get('state')
+    );
+  }
+
 
   /**
    * {@inheritdoc}
@@ -33,6 +67,21 @@ class WorkbenchAccessConfigForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config('workbench_access.settings');
+    $form['label'] = array(
+      '#type' => 'textfield',
+      '#size' => 32,
+      '#title' => t('Access group label'),
+      '#default_value' => $config->get('label', 'Section'),
+      '#description' => t('Label shown to define a Workbench Access control group.'),
+    );
+    $form['plural_label'] = array(
+      '#type' => 'textfield',
+      '#size' => 32,
+      '#title' => t('Access group label (plural form)'),
+      '#default_value' => $config->get('plural_label', 'Sections'),
+      '#description' => t('Label shown to define a set of Workbench Access control groups.'),
+    );
 
     return parent::buildForm($form, $form_state);
   }
@@ -41,15 +90,11 @@ class WorkbenchAccessConfigForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    /*
     $this->config('workbench_access.settings')
-      ->set('cache.page.max_age', $form_state->getValue('page_cache_maximum_age'))
-      ->set('css.preprocess', $form_state->getValue('preprocess_css'))
-      ->set('js.preprocess', $form_state->getValue('preprocess_js'))
+      ->set('label', $form_state->getValue('label'))
+      ->set('plural_label', $form_state->getValue('plural_label'))
       ->save();
-    */
     parent::submitForm($form, $form_state);
   }
-
 
 }
