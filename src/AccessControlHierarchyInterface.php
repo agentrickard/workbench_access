aed094c635869b5471e97c8761b6177f913e8842
agentrickard <agentrickard@gmail.com>
2015-05-29 18:49:43 -0400

    Stubs out an interface for hierarchies.

--- src/AccessControlHierarchyInterface.php
+++ src/AccessControlHierarchyInterface.php
@@ -12,4 +12,46 @@ namespace Drupal\workbench_access;
  */
 interface AccessControlHierarchyInterface {
 
+  /**
+   * Returns the label for a hierarchy.
+   */
+  public function label();
+
+  /**
+   * Returns the status of a hierarchy.
+   */
+  public function status();
+
+  /**
+   * Sets the status of a hierarchy.
+   */
+  public function setStatus();
+
+  /**
+   * Gets the entire hierarchy tree.
+   *
+   * @return array
+   */
+  public function getTree();
+
+  /**
+   * Loads a hierarchy definition.
+   */
+  public function load();
+
+  /**
+   * Provides configuration options.
+   */
+  public function configForm();
+
+  /**
+   * Validates configuration options.
+   */
+  public function configValidate();
+
+  /**
+   * Submits configuration options.
+   */
+  public function configSubmit();
+
 }
