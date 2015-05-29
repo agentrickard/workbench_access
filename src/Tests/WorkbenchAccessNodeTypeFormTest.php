5414af2b9a09dd6e908dff11391bed082b68b5ce
agentrickard <agentrickard@gmail.com>
2015-05-29 09:40:35 -0400

    Base tests complete for feature.

--- src/Tests/WorkbenchAccessNodeTypeFormTest.php
+++ src/Tests/WorkbenchAccessNodeTypeFormTest.php
@@ -8,9 +8,10 @@
 namespace Drupal\workbench_access\Tests;
 
 use Drupal\simpletest\WebTestBase;
+use Drupal\node\NodeTypeInterface;
 
 /**
- * Add configuration options to node types.
+ * Adds configuration options to node types.
  *
  * @group workbench_access
  */
@@ -38,7 +39,6 @@ class WorkbenchAccessNodeTypeFormTest extends WebTestBase {
     $this->editor = $this->drupalCreateUser(array(
       'access administration pages',
       'administer content types',
-      'administer menu',
       'create page content',
       'edit any page content',
       'delete any page content',
@@ -50,19 +50,37 @@ class WorkbenchAccessNodeTypeFormTest extends WebTestBase {
    * Test that the Workbench Access setting applies properly.
    */
   public function testWorkbenchAccessNodeTypeForm() {
+    // The form text string to find.
+    $string = 'Enable Workbench Access control for Basic page content.';
+    // The form path.
+    $path = 'admin/structure/types/manage/page';
 
     // Get the page content type settings.
+    $this->drupalGet($path);
 
     // Workbench Access should not be visible.
+    $this->assertNoRaw($string);
 
     // Set permission to 'administer workbench access'.
+    user_role_grant_permissions(DRUPAL_AUTHENTICATED_RID, array('administer workbench access'));
 
     // Workbench Access should be visible.
-
-    // Save the form.
-
-    // Test the setting in the form.
+    $this->drupalGet('admin/structure/types/manage/page');
+    $this->assertRaw($string);
 
     // Test the setting on the entity.
+    $type = entity_load('node_type', 'page');
+    $status = $type->getThirdPartySetting('workbench_access', 'workbench_access_status', 0);
+    $this->assertTrue($status === 0, 'Access control status is set to zero (off).');
+
+    // Save the form.
+    $edit['workbench_access_status'] = 1;
+    $this->drupalPostForm($path, $edit, 'Save content type');
+
+    // Test the setting on the entity.
+    $type = entity_load('node_type', 'page');
+    $status = $type->getThirdPartySetting('workbench_access', 'workbench_access_status', 0);
+    $this->assertTrue($status === 1, 'Access control status is set to one (on).');
+
   }
 }
