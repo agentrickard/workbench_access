374bba61d100a817af554f47a03419767b75767b
agentrickard <agentrickard@gmail.com>
2015-05-30 11:48:42 -0400

    Adds a stub manager interface for loading access rules.

---
+++ src/WorkbenchAccessManager.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\workbench_access\WorkbenchAccessManager.
+ */
+
+namespace Drupal\workbench_access;
+
+use Drupal\workbench_access\WorkbenchAccessManagerInterface;
+
+class WorkbenchAccessManager implements WorkbenchAccessManagerInterface {
+
+}
---
+++ src/WorkbenchAccessManagerInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\workbench_access\WorkbenchAccessManagerInterface.
+ */
+
+namespace Drupal\workbench_access;
+
+class WorkbenchAccessManagerInterface {
+
+}
