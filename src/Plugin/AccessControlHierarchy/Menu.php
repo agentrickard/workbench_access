c68fd546f8b4ae4be4321050d401cb0ed7e24ff2
agentrickard <agentrickard@gmail.com>
2015-06-01 13:25:50 -0400

    Working model for tree generation.

--- src/Plugin/AccessControlHierarchy/Menu.php
+++ src/Plugin/AccessControlHierarchy/Menu.php
@@ -8,6 +8,11 @@
 namespace Drupal\workbench_access\Plugin\AccessControlHierarchy;
 
 use Drupal\workbench_access\AccessControlHierarchyBase;
+use Drupal\Core\Menu\MenuLinkManagerInterface;
+use Drupal\Core\Menu\MenuLinkTreeElement;
+use Drupal\Core\Menu\MenuLinkTreeInterface;
+use Drupal\Core\Menu\MenuTreeParameters;
+use Drupal\Core\Menu\MenuLinkInterface;
 
 /**
  * Defines a hierarchy based on a Menu.
@@ -22,5 +27,35 @@ use Drupal\workbench_access\AccessControlHierarchyBase;
  * )
  */
 class Menu extends AccessControlHierarchyBase {
+
+  /**
+   * @inheritdoc
+   */
+  public function getTree() {
+    $config = $this->config('workbench_access.settings');
+    $parents = $config->get('parents');
+    $tree = array();
+    $this->menuTree = \Drupal::getContainer()->get('menu.link_tree');
+    foreach ($parents as $id => $label) {
+      $tree[$id][$id] = array(
+        'label' => $label,
+        'depth' => 0,
+        'parent' => '',
+        'weight' => 0,
+        'description' => $label,
+      );
+      $data = $this->menuTree->load($id, new MenuTreeParameters());
+      foreach ($data as $link_id => $link) {
+        $tree[$id][$link_id] = array(
+          'label' => $link->link->getTitle(),
+          'depth' => $link->depth,
+          'parent' => $link->link->getParent(),
+          'weight' => $link->link->getWeight(),
+          'description' => $link->link->getDescription(),
+        );
+      }
+    }
+    return $tree;
+  }
 
 }
