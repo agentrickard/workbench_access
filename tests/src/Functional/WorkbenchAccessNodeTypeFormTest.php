1b6383672fc6724c251581ed8a29a8c6ef63bead
Matt Carmichael <mattcarmichael@palantirs-MacBook-Pro-2.local>
2015-06-03 14:18:23 -0500

    refs 43719: Moving the simpletest file to the new Functional test directory

---
+++ tests/src/Functional/WorkbenchAccessNodeTypeFormTest.php
@@ -0,0 +1,86 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\workbench_access\Tests\WorkbenchAccessNodeTypeFormTest
+ */
+
+namespace Drupal\Tests\simpletest\Functional;
+
+use Drupal\simpletest\BrowserTestBase;
+use Drupal\node\NodeTypeInterface;
+
+/**
+ * Adds configuration options to node types.
+ *
+ * @group workbench_access
+ */
+class WorkbenchAccessNodeTypeFormTest extends BrowserTestBase {
+
+  /**
+   * An editor user.
+   *
+   * @var \Drupal\user\UserInterface
+   */
+  protected $editor;
+
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  public static $modules = array('workbench_access', 'node');
+
+  protected function setUp() {
+    parent::setUp();
+
+    $this->drupalCreateContentType(array('type' => 'page', 'name' => 'Basic page'));
+
+    $this->editor = $this->drupalCreateUser(array(
+      'access administration pages',
+      'administer content types',
+      'create page content',
+      'edit any page content',
+      'delete any page content',
+    ));
+    $this->drupalLogin($this->editor);
+  }
+
+  /**
+   * Test that the Workbench Access setting applies properly.
+   */
+  public function testWorkbenchAccessNodeTypeForm() {
+    // The form text string to find.
+    $string = 'Enable Workbench Access control for Basic page content.';
+    // The form path.
+    $path = 'admin/structure/types/manage/page';
+
+    // Get the page content type settings.
+    $this->drupalGet($path);
+
+    // Workbench Access should not be visible.
+    $this->assertNoText($string);
+
+    // Set permission to 'administer workbench access'.
+    user_role_grant_permissions(DRUPAL_AUTHENTICATED_RID, array('administer workbench access'));
+
+    // Workbench Access should be visible.
+    $this->drupalGet('admin/structure/types/manage/page');
+    $this->assertText($string);
+
+    // Test the setting on the entity.
+    $type = entity_load('node_type', 'page');
+    $status = $type->getThirdPartySetting('workbench_access', 'workbench_access_status', 0);
+    $this->assertTrue($status === 0, 'Access control status is set to zero (off).');
+
+    // Save the form.
+    $edit['workbench_access_status'] = 1;
+    $this->drupalPostForm($path, $edit, 'Save content type');
+
+    // Test the setting on the entity.
+    $type = entity_load('node_type', 'page');
+    $status = $type->getThirdPartySetting('workbench_access', 'workbench_access_status', 0);
+    $this->assertTrue($status == 1, 'Access control status is set to one (on).');
+
+  }
+}
