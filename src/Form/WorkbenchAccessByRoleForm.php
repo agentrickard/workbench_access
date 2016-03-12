821e2bd2afed5ac8e24d1fb147d18fa0cc0935a6
agentrickard <agentrickard@gmail.com>
2016-03-12 14:22:11 -0500

    Add sections by Role form working.

--- src/Form/WorkbenchAccessByRoleForm.php
+++ src/Form/WorkbenchAccessByRoleForm.php
@@ -68,13 +68,56 @@ class WorkbenchAccessByRoleForm extends FormBase {
    */
   public function buildForm(array $form, FormStateInterface $form_state, $id = NULL) {
     $element = $this->manager->getElement($id);
+    $existing_roles = $this->manager->getRoles($id);
+    $potential_roles = $this->manager->getPotentialRoles($id);
+
+    $form['existing_roles'] = ['#type' => 'value', '#value' => $existing_roles];
+    $form['section_id'] = ['#type' => 'value', '#value' => $id];
+    if (!$existing_roles) {
+      $text = $this->t('There are no roles assigned to the %label section.', array('%label' => $element['label']));
+      $form['help'] = array(
+        '#type' => 'markup',
+        '#markup' => '<p>' . $text . '</p>',
+      );
+    }
+
+    if ($potential_roles) {
+      $form['roles'] = array(
+        '#title' => $this->t('Roles for the %label section.', array('%label' => $element['label'])),
+        '#type' => 'checkboxes',
+        '#options' => $potential_roles,
+        '#default_value' => array_keys($existing_roles),
+      );
+      $form['actions'] = array('#type' => 'actions');
+      $form['actions']['submit'] = array('#type' => 'submit', '#value' => $this->t('Submit'));
+    }
+    else {
+      $form['message'] = array(
+        '#type' => 'markup',
+        '#markup' => '<p>' . $this->t('There are no addtional users that can be added to the %label section', array('%label' => $element['label'])) . '</p>',
+      );
+    }
+
+    return $form;
   }
 
   /**
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-
+    $roles = $form_state->getValue('roles');
+    $existing_roles = $form_state->getValue('existing_roles');
+    $id = $form_state->getValue('section_id');
+    foreach ($roles as $role_id => $value) {
+      // Add user to section.
+      if ($value && !isset($existing_roles[$role_id])) {
+        $this->manager->addRole($role_id, array($id));
+      }
+      // Remove user from section.
+      if (!$value && isset($existing_roles[$role_id])) {
+        $this->manager->removeRole($role_id, array($id));
+      }
+    }
   }
 
   public function pageTitle($id) {
--- src/WorkbenchAccessManager.php
+++ src/WorkbenchAccessManager.php
@@ -17,6 +17,7 @@ use Drupal\Core\Session\AccountInterface;
 use Drupal\user\RoleInterface;
 use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Entity\FieldableEntityInterface;
+use Drupal\Core\State\StateInterface;
 
 class WorkbenchAccessManager extends DefaultPluginManager implements WorkbenchAccessManagerInterface {
   use StringTranslationTrait;
@@ -90,7 +91,11 @@ class WorkbenchAccessManager extends DefaultPluginManager implements WorkbenchAc
   }
 
   public function addRole($role_id, $sections = array()) {
-
+    $settings = \Drupal::state()->get('workbench_access_roles_' . $role_id, array());
+    foreach ($sections as $id) {
+      $settings[$id] = 1;
+    }
+    \Drupal::state()->set('workbench_access_roles_' . $role_id, $settings);
   }
 
   public function addEntity($entity_id, $entity_type, $sections = array()) {
@@ -109,7 +114,13 @@ class WorkbenchAccessManager extends DefaultPluginManager implements WorkbenchAc
   }
 
   public function removeRole($role_id, $sections = array()) {
-
+    $settings = \Drupal::state()->get('workbench_access_roles_' . $role_id, array());
+    foreach ($sections as $id) {
+      if (isset($settings[$id])) {
+        unset($settings[$id]);
+      }
+    }
+    \Drupal::state()->set('workbench_access_roles_' . $role_id, $settings);
   }
 
   public function removeEntity($entity_id, $entity_type, $sections = array()) {
@@ -120,6 +131,7 @@ class WorkbenchAccessManager extends DefaultPluginManager implements WorkbenchAc
     $users = \Drupal::entityQuery('user')
       ->condition(WORKBENCH_ACCESS_FIELD, $id)
       ->condition('status', 1)
+      ->sort('name')
       ->execute();
     return $this->filterByPermission($users);
   }
@@ -137,6 +149,7 @@ class WorkbenchAccessManager extends DefaultPluginManager implements WorkbenchAc
     */
     $users = \Drupal::entityQuery('user')
       ->condition('status', 1)
+      ->sort('name')
       ->execute();
     return $this->filterByPermission($users);
   }
@@ -153,6 +166,24 @@ class WorkbenchAccessManager extends DefaultPluginManager implements WorkbenchAc
   }
 
   public function getRoles($id) {
-
+    $list = [];
+    $roles = \Drupal::entityManager()->getStorage('user_role')->loadMultiple();
+    foreach ($roles as $rid => $role) {
+      $settings = \Drupal::state()->get('workbench_access_roles_' . $rid, array());
+      if (!empty($settings[$id])) {
+        $list[$rid] = $role->label();
+      }
+    }
+    return $list;
   }
+
+  public function getPotentialRoles($id) {
+    $list = [];
+    $roles = \Drupal::entityManager()->getStorage('user_role')->loadMultiple();
+    foreach ($roles as $rid => $role) {
+      $list[$rid] = $role->label();
+    }
+    return $list;
+  }
+
 }
