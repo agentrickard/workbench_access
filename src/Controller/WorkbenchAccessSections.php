c083db67098ba4b61417fd39bcd376ac40cd5ce0
agentrickard <agentrickard@gmail.com>
2016-04-07 18:05:14 -0400

    Prevent sections page from loading with no scheme.

--- src/Controller/WorkbenchAccessSections.php
+++ src/Controller/WorkbenchAccessSections.php
@@ -21,31 +21,38 @@ class WorkbenchAccessSections extends ControllerBase {
    */
   public function page() {
     $config = $this->config('workbench_access.settings');
-    $scheme_id = $config->get('scheme');
-    $this->manager = \Drupal::getContainer()->get('plugin.manager.workbench_access.scheme');
-    $scheme = $this->manager->getScheme($scheme_id);
-    $parents = $config->get('parents');
-    $tree = $scheme->getTree();
-    $list = '';
-    foreach ($parents as $id => $label) {
-      // @TODO: Move to a theme function?
-      // @TODO: format plural
-      foreach ($tree[$id] as $iid => $item) {
-        $editor_count = count($this->manager->getEditors($iid));
-        $role_count = count($this->manager->getRoles($iid));
-        $row = [];
-        $row[] = str_repeat('-', $item['depth']) . ' ' . $item['label'];
-        $row[] = \Drupal::l($this->t('@count editors', ['@count' => $editor_count]), Url::fromRoute('workbench_access.by_user', array('id' => $iid))); // List of all editors.
-        $row[] = \Drupal::l($this->t('@count roles', ['@count' => $role_count]), Url::fromRoute('workbench_access.by_role', array('id' => $iid))); // List of all editors.
-        $rows[] = $row;
+    if ($scheme_id = $config->get('scheme')) {
+      $this->manager = \Drupal::getContainer()->get('plugin.manager.workbench_access.scheme');
+        $scheme = $this->manager->getScheme($scheme_id);
+      $parents = $config->get('parents');
+      $tree = $scheme->getTree();
+      $list = '';
+      foreach ($parents as $id => $label) {
+        // @TODO: Move to a theme function?
+        // @TODO: format plural
+        foreach ($tree[$id] as $iid => $item) {
+          $editor_count = count($this->manager->getEditors($iid));
+          $role_count = count($this->manager->getRoles($iid));
+          $row = [];
+          $row[] = str_repeat('-', $item['depth']) . ' ' . $item['label'];
+          $row[] = \Drupal::l($this->t('@count editors', ['@count' => $editor_count]), Url::fromRoute('workbench_access.by_user', array('id' => $iid))); // List of all editors.
+          $row[] = \Drupal::l($this->t('@count roles', ['@count' => $role_count]), Url::fromRoute('workbench_access.by_role', array('id' => $iid))); // List of all editors.
+          $rows[] = $row;
+        }
       }
-    }
 
-    $build = array(
-      '#type' => 'table',
-      '#header' => array($config->get('plural_label'), t('Editors'), t('Roles')),
-      '#rows' => $rows,
-    );
+      $build = array(
+        '#type' => 'table',
+        '#header' => array($config->get('plural_label'), t('Editors'), t('Roles')),
+        '#rows' => $rows,
+      );
+    }
+    else {
+      $build = array(
+        '#type' => 'markup',
+        '#markup' => $this->t('No sections are available.'),
+      );
+    }
     return $build;
   }
 }
