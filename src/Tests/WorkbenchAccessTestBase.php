f9adf60e6658921c62792ec7a22b0c2324484c34
agentrickard <agentrickard@gmail.com>
2016-04-20 14:21:14 -0400

    Add the manager to the test base.

--- src/Tests/WorkbenchAccessTestBase.php
+++ src/Tests/WorkbenchAccessTestBase.php
@@ -21,22 +21,47 @@ abstract class WorkbenchAccessTestBase extends WebTestBase {
    *
    * @var array
    */
-  public static $modules = array('workbench_access', 'node', 'taxonomy', 'options');
+  public static $modules = array('workbench_access', 'node', 'taxonomy', 'menu_ui', 'options');
+
+  /**
+   * The Workbench Access manager class.
+   *
+   * @var Drupal\workbench_access\WorkbenchAccessManager
+   */
+  protected $manager;
 
   protected function setUp() {
     parent::setUp();
 
+    $this->drupalCreateContentType(array('type' => 'article', 'name' => 'Article'));
     $this->drupalCreateContentType(array('type' => 'page', 'name' => 'Basic page'));
 
     // Install our base taxonomy.
     $this->installTaxonomy();
 
+    // Install a base menu.
+    $this->installMenu();
+
+    // Instantiate the manager.
+    $this->manager = \Drupal::getContainer()->get('plugin.manager.workbench_access.scheme');
+
   }
 
+  /**
+   * Loads and installs the test taxonomy from drush.
+   */
   public function installTaxonomy() {
     $file = DRUPAL_ROOT . '/' . drupal_get_path('module', 'workbench_access') . "/workbench_access.drush.inc";
     require_once $file;
     drush_workbench_access_test();
   }
+
+  /**
+   * Loads and installs the test menu.
+   */
+  public function installMenu() {
+    // @TODO.
+  }
+
 
 }
