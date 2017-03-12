8219918082e163fe3fd25817dd82360b88581cd1
Zombienaute <Zombienaute@1036766.no-reply.drupal.org>
2017-03-12 13:54:28 -0400

    Issue #2858283 by acbramley: Users with more than 1 role section only see a single section on node form

---
+++ tests/src/Functional/NodeFormTest.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Drupal\Tests\workbench_access\Functional;
+
+use Drupal\taxonomy\Entity\Term;
+use Drupal\Tests\BrowserTestBase;
+
+/**
+ * Tests for the node form.
+ *
+ * @group workbench_access
+ */
+class NodeFormTest extends BrowserTestBase {
+
+  use WorkbenchAccessTestTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static $modules = [
+    'workbench_access',
+    'node',
+    'taxonomy',
+    'options',
+    'user',
+    'system',
+  ];
+
+  /**
+   * Tests that the user can see all valid options on the node form.
+   */
+  public function testNodeForm() {
+    // Set up a content type, taxonomy field, and taxonomy scheme.
+    $node_type = $this->setUpContentType();
+    $vocab = $this->setUpVocabulary();
+    $this->setUpTaxonomyField($node_type, $vocab);
+    $this->setUpTaxonomyScheme($node_type, $vocab);
+    // Set up an editor and log in as them.
+    $editor = $this->setUpEditorUser();
+    $this->drupalLogin($editor);
+
+    // Set up some roles and terms for this test.
+    // Create terms and roles.
+    $staff_term = Term::create([
+      'vid' => $vocab->id(),
+      'name' => 'Staff',
+    ]);
+    $staff_term->save();
+    $super_staff_term = Term::create([
+      'vid' => $vocab->id(),
+      'name' => 'Super staff',
+    ]);
+    $super_staff_term->save();
+
+    $staff_rid = $this->createRole([], 'staff');
+    $super_staff_rid = $this->createRole([], 'super_staff');
+    // Set the role -> term mapping.
+    \Drupal::state()->set('workbench_access_roles_' . $staff_rid, [$staff_term->id() => 1]);
+    \Drupal::state()->set('workbench_access_roles_' . $super_staff_rid, [$super_staff_term->id() => 1]);
+
+    $web_assert = $this->assertSession();
+    $this->drupalGet('node/add/page');
+
+    // Assert we can't see the options yet.
+    $web_assert->optionNotExists(WORKBENCH_ACCESS_FIELD, $staff_term->getName());
+    $web_assert->optionNotExists(WORKBENCH_ACCESS_FIELD, $super_staff_term->getName());
+
+    // Add the staff role and check the option exists.
+    $editor->addRole($staff_rid);
+    $editor->save();
+    $this->drupalGet('node/add/page');
+    $web_assert->optionExists(WORKBENCH_ACCESS_FIELD, $staff_term->getName());
+
+    // Add the super staff role and check both options exist.
+    $editor->addRole($super_staff_rid);
+    $editor->save();
+    $this->drupalGet('node/add/page');
+    $web_assert->optionExists(WORKBENCH_ACCESS_FIELD, $staff_term->getName());
+    $web_assert->optionExists(WORKBENCH_ACCESS_FIELD, $super_staff_term->getName());
+  }
+
+}
---
+++ tests/src/Functional/WorkbenchAccessTestTrait.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace Drupal\Tests\workbench_access\Functional;
+
+use Drupal\Core\Entity\Entity\EntityFormDisplay;
+use Drupal\field\Entity\FieldConfig;
+use Drupal\field\Entity\FieldStorageConfig;
+use Drupal\node\Entity\NodeType;
+use Drupal\taxonomy\Entity\Vocabulary;
+
+/**
+ * Contains helper classes for tests to set up various configuration.
+ */
+trait WorkbenchAccessTestTrait {
+
+  /**
+   * Set up a content type with workbench access enabled.
+   *
+   * @return \Drupal\node\Entity\NodeType
+   *   The node type entity.
+   */
+  public function setUpContentType() {
+    $node_type = $this->createContentType(['type' => 'page']);
+    $node_type->setThirdPartySetting('workbench_access', 'workbench_access_status', 1);
+    $node_type->save();
+
+    return $node_type;
+  }
+
+  /**
+   * Create a test vocabulary.
+   *
+   * @return \Drupal\taxonomy\Entity\Vocabulary
+   *   The vocabulary entity.
+   */
+  public function setUpVocabulary() {
+    $vocab = Vocabulary::create(['vid' => 'workbench_access', 'name' => 'Access']);
+    $vocab->save();
+    return $vocab;
+  }
+
+  /**
+   * Sets up an access taxonomy field for a given vocab on a given node type.
+   *
+   * @param \Drupal\node\Entity\NodeType $node_type
+   *   The node type to create the field on.
+   * @param \Drupal\taxonomy\Entity\Vocabulary $vocab
+   *   The vocab to create the field against.
+   */
+  public function setUpTaxonomyField(NodeType $node_type, Vocabulary $vocab) {
+    // Create workbench access storage for node.
+    $field_storage = FieldStorageConfig::create([
+      'field_name' => WORKBENCH_ACCESS_FIELD,
+      'entity_type' => 'node',
+      'type' => 'entity_reference',
+      'cardinality' => 1,
+      'settings' => [
+        'target_type' => 'taxonomy_term',
+      ],
+    ]);
+    $field_storage->save();
+    // Create an instance of the access field on the content type.
+    FieldConfig::create([
+      'field_storage' => $field_storage,
+      'bundle' => $node_type->id(),
+      'settings' => [
+        'handler' => 'default:taxonomy_term',
+        'handler_settings' => [
+          'target_bundles' => [
+            $vocab->id() => $vocab->id(),
+          ],
+        ],
+      ],
+    ])->save();
+    // Set the field to display as a dropdown on the form.
+    $form_display = EntityFormDisplay::load('node.' . $node_type->id() . '.default');
+    $form_display->setComponent(WORKBENCH_ACCESS_FIELD, ['type' => 'options_select']);
+    $form_display->save();
+  }
+
+  /**
+   * Sets up a user with an editor role that has access to content.
+   *
+   * @return \Drupal\user\Entity\User
+   *   The user entity.
+   */
+  public function setUpEditorUser() {
+    $editor_rid = $this->createRole([
+      'access administration pages',
+      'create page content',
+      'edit any page content',
+      'delete any page content',
+    ], 'editor');
+
+    $user = $this->createUser();
+    $user->addRole($editor_rid);
+    $user->save();
+    return $user;
+  }
+
+  /**
+   * Sets up a taxonomy scheme for a given node type.
+   *
+   * @param \Drupal\taxonomy\Entity\Vocabulary $vocab
+   *   The vocab to use for the scheme.
+   */
+  public function setUpTaxonomyScheme(NodeType $node_type, Vocabulary $vocab) {
+    $config = \Drupal::configFactory()->getEditable('workbench_access.settings');
+    $config->set('scheme', 'taxonomy');
+    $config->set('parents', [$vocab->id() => $vocab->id()]);
+    $fields['node'][$node_type->id()] = WORKBENCH_ACCESS_FIELD;
+    $config->set('fields', $fields);
+    $config->save();
+  }
+
+}
