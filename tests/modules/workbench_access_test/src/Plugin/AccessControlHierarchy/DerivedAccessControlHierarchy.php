8be41c05150ea95c8ce31408a037d8b6adf6decf
larowlan <larowlan@395439.no-reply.drupal.org>
2017-03-13 12:52:07 +1000

    Issue #2860053 by larowlan: WorkbenchAccessManager::getSchemes doesn't work with plugin derivatives and mis-renders TranslateableMarkup objects

---
+++ tests/modules/workbench_access_test/src/Plugin/AccessControlHierarchy/DerivedAccessControlHierarchy.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Drupal\workbench_access_test\Plugin\AccessControlHierarchy;
+
+use Drupal\workbench_access\AccessControlHierarchyBase;
+use Drupal\workbench_access\WorkbenchAccessManagerInterface;
+
+/**
+ * Defines a hierarchy based on an entity hierarchy field.
+ *
+ * @AccessControlHierarchy(
+ *   id = "workbench_access_test_derived",
+ *   module = "workbench_access_test",
+ *   deriver = "Drupal\workbench_access_test\Plugin\Derivative\DerivedAccessControlPlugins",
+ *   label = @Translation("Derived plugins"),
+ *   description = @Translation("Uses derivatives for plugins.")
+ * )
+ */
+class DerivedAccessControlHierarchy extends AccessControlHierarchyBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFields($entity_type, $bundle, $parents) {
+    return ['uid' => 'User'];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function alterOptions($field, WorkbenchAccessManagerInterface $manager) {
+    return $field;
+  }
+
+}
