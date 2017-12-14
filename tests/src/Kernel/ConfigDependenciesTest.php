88f5a75ca1c0cf5700d29cdbbfb5023cd7ee9e86
Lee Rowlands <lee.rowlands@previousnext.com.au>
2017-12-14 10:01:21 +1000

    Issue #2790107 by larowlan: Workbench Access for all content entity types - test for handler dependencies

---
+++ tests/src/Kernel/ConfigDependenciesTest.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Drupal\Tests\workbench_access\Unit;
+
+use Drupal\KernelTests\KernelTestBase;
+use Drupal\Tests\node\Traits\ContentTypeCreationTrait;
+use Drupal\Tests\workbench_access\Functional\WorkbenchAccessTestTrait;
+use Drupal\workbench_access\Plugin\views\field\Section;
+
+/**
+ * Defines a class for testing config dependencies.
+ */
+class ConfigDependenciesTest extends KernelTestBase {
+
+  use WorkbenchAccessTestTrait;
+  use ContentTypeCreationTrait;
+
+  /**
+   * Access vocabulary.
+   *
+   * @var \Drupal\taxonomy\VocabularyInterface
+   */
+  protected $vocabulary;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = [
+    'node',
+    'text',
+    'system',
+    'user',
+    'workbench_access',
+    'field',
+    'filter',
+    'taxonomy',
+    'options',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+    $this->installEntitySchema('node');
+    $this->installConfig(['filter', 'node', 'workbench_access']);
+    $this->installEntitySchema('user');
+    $this->installEntitySchema('taxonomy_term');
+    $this->installSchema('system', ['key_value', 'sequences']);
+    module_load_install('workbench_access');
+    workbench_access_install();
+    $node_type = $this->createContentType(['type' => 'page']);
+    $this->createContentType(['type' => 'article']);
+    $this->vocabulary = $this->setUpVocabulary();
+    $this->setUpTaxonomyFieldForEntityType('node', $node_type->id(), $this->vocabulary->id());
+    $this->setUpTaxonomyScheme($node_type, $this->vocabulary);
+  }
+
+  /**
+   * Tests views field dependencies.
+   */
+  public function testViewsFieldDependencies() {
+    $definition = [
+      'scheme' => 'editorial_section',
+    ];
+    $handler = Section::create($this->container, [], 'section:editorial_section', $definition);
+
+    $dependencies = $handler->calculateDependencies();
+    $this->assertEquals(['config' => ['workbench_access.access_scheme.editorial_section']], $dependencies);
+  }
+
+}
