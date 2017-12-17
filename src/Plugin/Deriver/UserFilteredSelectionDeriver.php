d97868fae490c91541e28286d63481a2b8bb0c1b
Lee Rowlands <lee.rowlands@previousnext.com.au>
2017-12-18 08:59:50 +1000

    Issue #2790107 by larowlan: Workbench Access for all content entity types -Issue #2790107 by larowlan: Workbench Access for all content entity types - removed unused use statments

--- src/Plugin/Deriver/UserFilteredSelectionDeriver.php
+++ src/Plugin/Deriver/UserFilteredSelectionDeriver.php
@@ -2,12 +2,6 @@
 
 namespace Drupal\workbench_access\Plugin\Deriver;
 
-use Drupal\Component\Plugin\Derivative\DeriverBase;
-use Drupal\Core\Entity\EntityTypeManagerInterface;
-use Drupal\Core\Plugin\Discovery\ContainerDeriverInterface;
-use Drupal\Core\StringTranslation\TranslatableMarkup;
-use Symfony\Component\DependencyInjection\ContainerInterface;
-
 /**
  * Defines a selection handler deriver for filtered users.
  */
