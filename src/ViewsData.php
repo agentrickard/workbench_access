5b942729a92c00fdf72cd5b3ce7f694fd40abd37
Ken Rickard <agentrickard@gmail.com>
2018-02-12 14:47:01 -0500

    Comments for TODO with regard to Views.

--- src/ViewsData.php
+++ src/ViewsData.php
@@ -65,6 +65,22 @@ class ViewsData {
     $scheme_storage = $this->entityTypeManager->getStorage('access_scheme');
     foreach ($scheme_storage->loadMultiple() as $scheme) {
       // Do things?
+
+      /* Some notes for later:
+
+        - Decide which tables to JOIN to, if any.
+        - Decide which tables to form relationships for.
+        - Define any configuration that will be necessary. I suspect that
+          configuration per field will require specifying a specific scheme.
+        - Explore whether the views_data_alter hook is still needed. In fact,
+          we may need to do all of our computation there, with the exception
+          of the base relationships.
+        - However, if may be that the relationships require configuration.
+
+
+
+
+      */
     }
     return $data;
   }
