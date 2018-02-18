f73d095e03b703a34b0179e91eebbb15f747ec5f
Ken Rickard <agentrickard@gmail.com>
2018-02-18 10:23:39 -0500

    Change method signature of addUser().

--- src/UserSectionStorage.php
+++ src/UserSectionStorage.php
@@ -105,7 +105,7 @@ class UserSectionStorage implements UserSectionStorageInterface {
    *
    * @TODO: refactor.
    */
-  public function addUser(AccessSchemeInterface $scheme, $user_id, array $sections = []) {
+  public function addUser(AccessSchemeInterface $scheme, AccountInterface $account, array $sections = []) {
     foreach ($sections as $id) {
       // @TODO: This is tortured logic and probably much easier to handle.
       if ($section_association = $this->sectionStorage()->loadSection($scheme->id(), $id)) {
