16ac2ac9f0081a32af66751235376e51c3410524
Ken Rickard <agentrickard@gmail.com>
2018-03-01 10:21:15 -0500

    Base test pass.

--- tests/src/Functional/AssignUserFormTest.php
+++ tests/src/Functional/AssignUserFormTest.php
@@ -2,9 +2,10 @@
 
 namespace Drupal\Tests\workbench_access\Functional;
 
+use Drupal\Core\Url;
+use Drupal\Tests\BrowserTestBase;
 use Drupal\menu_link_content\Entity\MenuLinkContent;
 use Drupal\taxonomy\Entity\Term;
-use Drupal\Tests\BrowserTestBase;
 use Drupal\workbench_access\Entity\AccessSchemeInterface;
 use Drupal\Tests\workbench_access\Traits\WorkbenchAccessTestTrait;
 
@@ -33,35 +34,113 @@ class AssignUserFormTest extends BrowserTestBase {
   ];
 
   /**
-   * Tests that the correct users are displayed on the access by user form.
+   * Tests that the AssignUserForm works correctly.
    */
   public function testAssignUserForm() {
-    // Set up test taxonomy  scheme.
+    // Set up test taxonomy scheme.
     $node_type = $this->createContentType(['type' => 'page']);
     $vocab = $this->setUpVocabulary();
     $this->setUpTaxonomyFieldForEntityType('node', $node_type->id(), $vocab->id());
-    $taxonomy_scheme = $this->setUpTaxonomyScheme($node_type, $vocab);
+    $taxonomy_scheme = $this->setUpTaxonomyScheme($node_type, $vocab, 'taxonomy_section');
 
-    // Set up some roles and terms for this test.
+    // Create terms for the test.
     $staff_term = Term::create([
       'vid' => $vocab->id(),
       'name' => 'Staff',
     ]);
     $staff_term->save();
-    $section_id = $staff_term->id();
+    $super_staff_term = Term::create([
+      'vid' => $vocab->id(),
+      'name' => 'Super staff',
+    ]);
+    $super_staff_term->save();
+    $base_term = Term::create([
+      'vid' => $vocab->id(),
+      'name' => 'Editor',
+    ]);
+    $base_term->save();
 
     // Set up test menu scheme.
     $node_type = $this->createContentType(['type' => 'article']);
-    $menu_scheme = $this->setUpMenuScheme([$node_type->id()], ['main']);
+    $menu_scheme = $this->setUpMenuScheme([$node_type->id()], ['main'], 'menu_section');
 
-    // Create a menu link.
-    $link = MenuLinkContent::create([
-      'title' => 'Home',
+    // Set up some menu links for this test.
+    $staff_link = MenuLinkContent::create([
+      'title' => 'Link 1',
       'link' => [['uri' => 'route:<front>']],
       'menu_name' => 'main',
     ]);
-    $link->save();
-    $section_id = $link->id();
+    $staff_link->save();
+    $super_staff_link = MenuLinkContent::create([
+      'title' => 'Link 2',
+      'link' => [['uri' => 'route:<front>']],
+      'menu_name' => 'main',
+    ]);
+    $super_staff_link->save();
+    $base_link = MenuLinkContent::create([
+      'title' => 'Link 3',
+      'link' => [['uri' => 'route:<front>']],
+      'menu_name' => 'main',
+    ]);
+    $base_link->save();
+
+    // Set some users with permissions.
+    // Super admin.
+    $admin_rid = $this->createRole([
+      'access administration pages',
+      'assign workbench access',
+      'bypass workbench access',
+      'create page content',
+      'edit any page content',
+      'delete any page content',
+      'create article content',
+      'edit any article content',
+      'delete any article content',
+      'administer menu',
+      'use workbench access',
+      'access user profiles',
+    ], 'admin');
+    $admin_user = $this->createUserWithRole($admin_rid);
+
+    // Partial admin.
+    $partial_rid = $this->createRole([
+      'access administration pages',
+      'assign selected workbench access',
+      'create page content',
+      'edit any page content',
+      'delete any page content',
+      'create article content',
+      'edit any article content',
+      'delete any article content',
+      'administer menu',
+      'use workbench access',
+      'access user profiles',
+    ], 'partial');
+    $partial_user = $this->createUserWithRole($partial_rid);
+
+    // No admin.
+    $none_rid = $this->createRole([
+      'access administration pages',
+      'create page content',
+      'edit any page content',
+      'delete any page content',
+      'create article content',
+      'edit any article content',
+      'delete any article content',
+      'use workbench access',
+      'access user profiles',
+    ], 'none');
+    $none_user = $this->createUserWithRole($none_rid);
+
+    // Check page access.
+    $this->drupalLogin($admin_user);
+    $this->drupalGet(Url::fromRoute('entity.section_association.edit', ['user' => $none_user->id()]));
+    $assert = $this->assertSession();
+    $assert->statusCodeEquals(200);
+
+    // Check page options.
+    $assert->pageTextContains('Taxonomy sections');
+    $assert->pageTextContains('Menu sections');
   }
 
 }
