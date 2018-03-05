8d596d40c92f258fb87d2ac71883ba318a8626b7
Ken Rickard <agentrickard@gmail.com>
2018-03-05 14:46:57 -0500

    Cleanup SectionAssociationInterface.php

--- src/Entity/SectionAssociationInterface.php
+++ src/Entity/SectionAssociationInterface.php
@@ -23,6 +23,7 @@ interface SectionAssociationInterface extends ContentEntityInterface {
    * Returns an array of currently assigned user ids for the section.
    *
    * @return array
+   *   A positional array of user ids.
    */
   public function getCurrentUserIds();
 
@@ -30,6 +31,7 @@ interface SectionAssociationInterface extends ContentEntityInterface {
    * Returns an array of currently assigned role ids for the section.
    *
    * @return array
+   *   A positional array of role ids.
    */
   public function getCurrentRoleIds();
 
