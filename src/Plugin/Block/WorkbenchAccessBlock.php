58676b0103d6e61b9f815fd96e99a3a076178f34
Lee Rowlands <lee.rowlands@previousnext.com.au>
2019-11-19 06:32:22 +1000

    Use context definition instead of route match for cacheability metadata

--- src/Plugin/Block/WorkbenchAccessBlock.php
+++ src/Plugin/Block/WorkbenchAccessBlock.php
@@ -4,7 +4,11 @@ namespace Drupal\workbench_access\Plugin\Block;
 
 use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\Session\AccountInterface;
+use Drupal\node\NodeInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Creates a block to show editorial status.
@@ -12,17 +16,59 @@ use Drupal\Core\Session\AccountInterface;
  * @Block(
  *   id = "workbench_access_block",
  *   admin_label = @Translation("Workbench Access information")
+ *   context_definitions = {
+ *     "node" = @ContextDefinition("entity:node", label = @Translation("Current node"))
+ *   }
  * )
  */
-class WorkbenchAccessBlock extends BlockBase {
+class WorkbenchAccessBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * Entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Constructs a new WorkbenchAccessBlock.
+   *
+   * @param array $configuration
+   *   Configuration.
+   * @param string $plugin_id
+   *   ID.
+   * @param array|object $plugin_definition
+   *   Definition.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entityTypeManager
+   *   Entity type manager.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entityTypeManager) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->entityTypeManager = $entityTypeManager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity_type.manager')
+    );
+  }
 
   /**
    * {@inheritdoc}
    */
   public function build() {
     $build = [];
-    if ($node = \Drupal::routeMatch()->getParameter('node')) {
-      $scheme_storage = \Drupal::entityTypeManager()->getStorage('access_scheme');
+    // Using a context definition ensures the cacheability metadata from the
+    // node is applied to the block output.
+    // @see \Drupal\Core\Plugin\Context\ContextHandler::applyContextMapping
+    if (($node = $this->getContextValue('node')) && $node instanceof NodeInterface) {
+      $scheme_storage = $this->entityTypeManager->getStorage('access_scheme');
       if ($schemes = $scheme_storage->loadMultiple()) {
         /** @var \Drupal\workbench_access\Entity\AccessSchemeInterface $scheme */
         foreach ($schemes as $id => $scheme) {
@@ -39,9 +85,18 @@ class WorkbenchAccessBlock extends BlockBase {
           }
         }
       }
-      $build['#cache']['tags'] = ['node_list'];
     }
     return $build;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheContexts() {
+    $contexts = parent::getCacheContexts();
+    // The output varies per user permissions.
+    $contexts[] = 'user.permissions';
+    return $contexts;
   }
 
   /**
