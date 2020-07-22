3f58fa24004795148d0f01fd25fb05bdf576b212
damontgomery <damontgomery@1393336.no-reply.drupal.org>
2020-07-22 13:11:22 -0400

    Issue #2982550 by andy_w, damontgomery: Allow specific schemes to be applied to certain roles

--- src/Form/WorkbenchAccessConfigForm.php
+++ src/Form/WorkbenchAccessConfigForm.php
@@ -35,6 +35,12 @@ class WorkbenchAccessConfigForm extends ConfigFormBase {
       '#default_value' => $config->get('deny_on_empty'),
       '#description' => $this->t('For content under access control, deny access for any content not assigned to a section. This setting is off by default so that installing the module does not break existing site behavior.'),
     ];
+    $form['deny_strict'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Deny access if any scheme does not provide access'),
+      '#default_value' => $config->get('deny_strict'),
+      '#description' => $this->t('When multiple schemes apply, deny access if any scheme does not provide access. Without this checked, access is only denied if all schemes do not provide access.'),
+    ];
 
     return parent::buildForm($form, $form_state);
   }
@@ -45,6 +51,7 @@ class WorkbenchAccessConfigForm extends ConfigFormBase {
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $config = $this->config('workbench_access.settings');
     $config->set('deny_on_empty', $form_state->getValue('deny_on_empty'))->save();
+    $config->set('deny_strict', $form_state->getValue('deny_strict'))->save();
     parent::submitForm($form, $form_state);
   }
 
