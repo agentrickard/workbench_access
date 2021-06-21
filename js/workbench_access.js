5c8f8035f3ff21bf035bb7dffad2c10ad00ad7fa
agentrickard <agentrickard@20975.no-reply.drupal.org>
2021-06-21 13:23:24 -0400

    Issue #3219113 by agentrickard, prudloff, larowlan: Claro theme breaks workbench_access.js

--- js/workbench_access.js
+++ js/workbench_access.js
@@ -7,10 +7,11 @@
     attach: function (context, settings) {
       $('#edit-add', context).once('field-switch').each(function () {
         // We hide mass assign.
-        $('.form-item-editors-add-mass').hide();
+        $('.js-form-item-editors-add-mass').hide();
         $(this).find('.switch').click(function(e) {
-          $('.form-item-editors-add').toggle();
-          $('.form-item-editors-add-mass').toggle();
+          e.preventDefault();
+          $('.js-form-item-editors-add').toggle();
+          $('.js-form-item-editors-add-mass').toggle();
         })
       });
     }
